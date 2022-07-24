4447ccd9632896da74466d021efed291decccd0f
Stefan Tataran <fane_ecl@yahoo.com>
2022-07-24 13:49:04 +0300

    localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'personal-page';
+    title = $localize`Personal Page`;
 }
